66d686d6fcebc5cb2b6e1864aba3cb492ec574a5
praveenkumar2022344-dev <praveenkumar2022344@gmail.com>
2026-09-02 09:34:00 +0530

    Update server.py

--- server.py
+++ server.py
@@ -93,7 +93,7 @@ async def websocket_endpoint(websocket: WebSocket):
         
         # 2. Start Gemini AI stream
         response = await client.aio.models.generate_content_stream(
-            model='gemini-3.5-flash',
+            model='gemini-1.5-flash',
             contents=prompt
         )
         
